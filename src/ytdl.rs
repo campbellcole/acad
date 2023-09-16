24a1e3585b935d40b9d94d4479bcc2fb26a1ed57
campbellcole <10430178+campbellcole@users.noreply.github.com>
2023-09-16 16:00:06 -0500

    error handling for yt-dlp, plus debugging actual stdout

--- src/ytdl.rs
+++ src/ytdl.rs
@@ -53,7 +53,14 @@ pub async fn fetch_manifests(url: &str) -> Result<Vec<OrderedTrackManifest>> {
 
     let output = child.wait_with_output().await?;
 
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        return Err(eyre!("yt-dlp failed: {:?}", stderr));
+    }
+
     let stdout = String::from_utf8(output.stdout)?;
+
+    trace!("yt-dlp output: {}", stdout);
 
     let mut tracks = Vec::new();
 
